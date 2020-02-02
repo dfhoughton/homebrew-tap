df6aad5e4b0433a322d7b8cff5a8345d98927039
dfhoughton <dfhoughton@gmail.com>
2020-02-02 12:48:08 -0500

    trying to get Catalina bottle working again

--- Formula/jobrog.rb
+++ Formula/jobrog.rb
@@ -1,16 +1,15 @@
 class Jobrog < Formula
   desc "Command-line time tracking and note taking utility"
   homepage "https://github.com/dfhoughton/jobrog"
+  version "0.1.6"
   url "https://github.com/dfhoughton/jobrog/archive/0.1.6.1.tar.gz"
   sha256 "ab7bfab308f1962d157a03167aea3f00624b7be40fc39e8a547d57dfc44d022c"
-  version "0.1.6"
 
   bottle do
     root_url "https://dl.bintray.com/dfhoughton/bottles-tap"
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "0d2091977e8f62eae0bc91003dd953d3e96425146d8a8ba29a5eb52e1c92cde5" => :catalina
-    sha256 "bec3499431e0bae9634d1732a9d4e2e94d0e6c8db33875134fa2789c74228002" => :mojave
+    rebuild 3
+    sha256 "42b3fd3b4aebef485f1e9fa9231ac9f081c3cf33c26a281c2cfe30c919cd67f3" => :catalina
   end
 
   depends_on "rust" => :build
