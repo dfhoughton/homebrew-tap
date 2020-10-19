ab085a72aef1979148272c7b1303989fb5158d06
dfhoughton <dfhoughton@gmail.com>
2020-10-19 18:38:32 -0400

    0.3.4 changes

--- Formula/jobrog.rb
+++ Formula/jobrog.rb
@@ -1,13 +1,13 @@
 class Jobrog < Formula
   desc "Command-line time tracking and note taking utility"
   homepage "https://github.com/dfhoughton/jobrog"
-  url "https://github.com/dfhoughton/jobrog/archive/0.3.3.tar.gz"
-  sha256 "1e7e531e20bb003d9c9f4379b3fca389d419a5a29d8e6ecf243555acaecce79f"
+  url "https://github.com/dfhoughton/jobrog/archive/0.3.4.tar.gz"
+  sha256 "50fed0840a01cc45b69ba496be69bfc96010f349e6915fc8c1e34d6e1e72ffc4"
 
   bottle do
     root_url "https://dl.bintray.com/dfhoughton/bottles-tap"
     cellar :any_skip_relocation
-    sha256 "1e7e531e20bb003d9c9f4379b3fca389d419a5a29d8e6ecf243555acaecce79f" => :catalina
+    sha256 "50fed0840a01cc45b69ba496be69bfc96010f349e6915fc8c1e34d6e1e72ffc4" => :catalina
   end
 
   depends_on "rust" => :build
