64edf523f1e4fd3206331b1ddcf01e15010b9d5e
dfhoughton <dfhoughton@gmail.com>
2020-11-21 17:46:39 -0500

    explicitly specifying the version

--- Formula/jobrog.rb
+++ Formula/jobrog.rb
@@ -2,6 +2,7 @@ class Jobrog < Formula
   desc "Command-line time tracking and note taking utility"
   homepage "https://github.com/dfhoughton/jobrog"
   url "https://github.com/dfhoughton/jobrog/archive/1.0.0.tar.gz"
+  version "1.0.0"
   sha256 "0088becad1074a0e799b2511be763a1110a5003bdf6ca2b5202f8b09e43a2bb2"
 
   bottle do
