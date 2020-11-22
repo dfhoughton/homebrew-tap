545860ac6b82fd4aefe41759a3304c2b558b6bf0
dfhoughton <dfhoughton@gmail.com>
2020-11-22 10:37:47 -0500

    still trying to get the formula to work

--- Formula/jobrog.rb
+++ Formula/jobrog.rb
@@ -4,11 +4,11 @@ class Jobrog < Formula
   url "https://github.com/dfhoughton/jobrog/archive/1.0.0.tar.gz"
   sha256 "0088becad1074a0e799b2511be763a1110a5003bdf6ca2b5202f8b09e43a2bb2"
 
-#  bottle do
-#    root_url "https://dl.bintray.com/dfhoughton/bottles-tap"
-#    cellar :any_skip_relocation
-#    sha256 "0088becad1074a0e799b2511be763a1110a5003bdf6ca2b5202f8b09e43a2bb2" => :catalina
-#  end
+  #bottle do
+  #  root_url "https://dl.bintray.com/dfhoughton/bottles-tap"
+  #  cellar :any_skip_relocation
+  #  sha256 "0088becad1074a0e799b2511be763a1110a5003bdf6ca2b5202f8b09e43a2bb2" => :catalina
+  #end
 
   depends_on "rust" => :build
 
